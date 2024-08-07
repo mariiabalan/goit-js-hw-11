e2eb41de46c487636a76b756681faae69e72b8c8
mariiabalan <159541977+mariiabalan@users.noreply.github.com>
2024-08-07 14:47:07 +0200

    api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 
 export function searchGalleryQuery(query) {
     const URL = "https://pixabay.com/api/";
-    const API_KEY = "45091921-bafbe55c990439d7032dec8c8";
+    const API_KEY = "45320623-f265cdcadbbe2c6a410e5ea22";
 
     return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
         .then((response) => {
